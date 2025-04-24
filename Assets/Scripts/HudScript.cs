250ddf67b57ace18b764c095b10d149723d97cc8
Milan <davidovicmilan98@gmail.com>
2025-04-24 11:25:57 +0200

    slider_added

--- Assets/Scripts/HudScript.cs
+++ Assets/Scripts/HudScript.cs
@@ -4,12 +4,14 @@ using UnityEngine.UI;
 
 public class HudScript : MonoBehaviour
 {
+    public PlayerScript player;
     public TextMeshProUGUI Score;
     public Slider Cooldown;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-         
+        Cooldown.Value = player.CooldownPercentage();
+
     }
 
     // Update is called once per frame
