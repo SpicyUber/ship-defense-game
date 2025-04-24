eb1414234933adeaa80ab9b0eba24047570ddbcd
Echodart <yeahthisisanemaillol@gmail.com>
2025-04-24 17:54:51 +0200

    Rock Up And Down finished and Raycasting Logic
    note to self- put raycasting when spawning enemies when u make that, also the hitbox on enemyship is tiny...

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -14,27 +14,47 @@ public class Enemy : MonoBehaviour
     float distance;
     Vector3 movingDirection;
     float movingIntensity = 25f;
+    bool _is_on_distance = false;
 
-
-    void Start()
+  
+    private void Start()
     {
-        movingDirection = (player.position - transform.position).normalized;
         transform.LookAt(player);
+        //CannonObject.ShootAfterAnimation() // prvi argument normalizovana direkcija, a drugi obicna
     }
 
     void FixedUpdate()
     {
         distance = Vector3.Distance(player.position, transform.position);
+        movingDirection = (player.position - transform.position).normalized;
 
-        if (distance > minDistance)
+        //bedan pokusaj raycastinga
+
+        if (Physics.Raycast(transform.position, movingDirection, out RaycastHit hit))
+        {
+            //stavi ga kod spawnera
+            if (hit.collider.tag != "Player")
+            {
+                _is_on_distance = true;
+                rb.linearVelocity = Vector3.zero;
+            }
+        }
+
+        //provera uslova za kretanje i stizanje u aproksimitet igracu
+
+        if (distance > minDistance && !_is_on_distance)
         {
             rb.AddForce(movingDirection * movingIntensity, ForceMode.Force);
         }
-        else
+        else if(distance <= minDistance && (_is_on_distance || !_is_on_distance))
         {
             rb.linearVelocity = Vector3.zero;
+            _is_on_distance = true;
         }
     }
+
+
+  
 
     public void Sink(Vector3 ImpactWorldPosition) { }
     public void SpawnSelf() { }
