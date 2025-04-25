8b991ca7d77c83b92d659f36461e687642c1d934
Aleksandar Ilanković <ilankovicaleksandar@gmail.com>
2025-04-25 03:44:39 +0200

    Add Ball Prefab, Implement Shoot and ShootAfterAnimation methods
    Add Ball Prefab and implement Ball script. Also implement the Shoot and ShootAfterAnimation method along with helping methods and variables. Finally, add contextmenu for testing the two Cannon methods.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,15 +5,21 @@ public class Ball : MonoBehaviour
     public Rigidbody BallRigidbody;
     public bool IsCurrentlyFlying;
     public string Tag;
+    private MeshRenderer _ballModel;
+    private Collider _ballCollider;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        IsCurrentlyFlying = false;
+        _ballModel = GetComponentInChildren<MeshRenderer>();
+        _ballCollider = GetComponent<Collider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
+        _ballModel.enabled = IsCurrentlyFlying;
+        _ballCollider.enabled = IsCurrentlyFlying;
     }
 }
--- Assets/Scripts/Cannon.cs
+++ Assets/Scripts/Cannon.cs
@@ -10,15 +10,33 @@ public class Cannon : MonoBehaviour
     [Header("~ADDITIONAL ATTRIBUTES~")]
 
     public GameObject CannonModel;
+    public Transform ShootTransform;
+    public ParticleSystem ShootParticleSystem;
     private Material _cannonMaterial;
     public bool IsPlayerCannon;
     private Vector3 _startingModelScale;
     private Color _startingColor;
+    private float _shootAnimationCooldown = 2f;
+    private float _shootAnimationCooldownTimer = 2f;
+    private bool _shootAnimationIsOn = false;
 
     //Intensity variables
     private float _intensityDampener=100f;
     private const float _intensityLimiter=0.5f;
 
+    //Enemy shooting variables
+    public Vector3 ShootDirection;
+    public Vector3 ShootForce;
+
+
+    
+    
+   //test methods
+    [ContextMenu("EnemyShoot")]
+    void EnemyShotTest() { IsPlayerCannon = false; ShootAfterAnimation(ShootDirection, ShootForce); }
+
+    [ContextMenu("PlayerShoot")]
+    void PlayerShotTest() { IsPlayerCannon = true; Shoot(ShootDirection, ShootForce); }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -33,14 +51,70 @@ public class Cannon : MonoBehaviour
     void Update()
     {
         ConvertIntensityToVisuals();
+        ShootAnimation();
     }
 
-    public void ShootAfterAnimation(Vector3 direction, Vector3 force) { }
+    
+    public void ShootAfterAnimation(Vector3 direction, Vector3 force) { 
+    
+        if(IsPlayerCannon || _shootAnimationCooldownTimer< _shootAnimationCooldown)  return; 
 
-    public void Shoot(Vector3 direction, Vector3 force) { }
+      _shootAnimationIsOn=true;
+        _shootAnimationCooldownTimer=0;
+        ShootDirection=direction;
+        ShootForce=force;
+    }
+
+    
+    public void Shoot(Vector3 direction, Vector3 force) {
+        if (!IsPlayerCannon) { return; }
+        ActualShoot("Player");
+        CannonChargeIntensity = 0;
+    }
+
+    private void ActualShoot(string Tag) {
+        if(BallObject==null) return;
+        ShootExplosion();
+        BallObject.Tag = Tag;
+        BallObject.BallRigidbody.linearVelocity = Vector3.zero;
+        BallObject.IsCurrentlyFlying = true;
+        BallObject.transform.position = ShootTransform.position ;
+        BallObject.BallRigidbody.AddForce(ShootForce,ForceMode.VelocityChange);
+        
+    }
+
+    private void OnDisable()
+    {
+        CannonChargeIntensity=0;
+        if (BallObject == null) return;
+        BallObject.BallRigidbody.linearVelocity = Vector3.zero;
+        BallObject.IsCurrentlyFlying = false;
+    }
+
+    private void ShootAnimation()
+    {
+        if (!_shootAnimationIsOn) return;
+
+
+        CannonChargeIntensity = _intensityLimiter * _intensityDampener*(_shootAnimationCooldownTimer / _shootAnimationCooldown);
+
+        _shootAnimationCooldownTimer += Time.deltaTime;
+        if(_shootAnimationCooldownTimer >= _shootAnimationCooldown) { CannonChargeIntensity = 0; _shootAnimationIsOn = false; _shootAnimationCooldownTimer = _shootAnimationCooldown; ActualShoot("Enemy");  }
+
+    }
+
+    private void ShootExplosion()
+    { if (ShootParticleSystem == null) return;
+
+    ShootParticleSystem.Play();
+        
+
+        
+
+    }
 
     private void ConvertIntensityToVisuals() { 
-        if (!IsPlayerCannon) { return; }
+       if(CannonModel ==null || _cannonMaterial == null) return;    
 
         float cannonChargeIntensityAfterDampenerAndLimiter = ((CannonChargeIntensity / _intensityDampener)>_intensityDampener)? _intensityLimiter : (CannonChargeIntensity / _intensityDampener);
 
